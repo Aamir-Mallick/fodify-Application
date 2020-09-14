77702ceabbcff77400f3aa021e883561b3c126e9
Md Aamir Mallick <aamirmallick71@gmail.com>
2020-09-15 01:44:17 +0530

    adding cart login

--- src/components/cart/Dish .js
+++ src/components/cart/Dish .js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import swal from 'sweetalert';
+import swal from "sweetalert";
 import axios from "axios";
 import * as firebase from "firebase";
 import "../../config/fire";
@@ -26,13 +26,21 @@ class Dish extends Component {
   };
 
   componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          uid: user.uid,
+        });
+      }
+    });
+
     axios(
       `https://cors-anywhere.herokuapp.com/https://aamirmenusapi.herokuapp.com/menuitems`
     )
       .then((data) => {
         this.setState({
           dishData: data.data,
-        })
+        });
       })
       .catch((error) => {
         swal("Something went wrong!", error.message, "error");
@@ -59,9 +67,13 @@ class Dish extends Component {
   }
 
   addToCartHandlar = (item) => {
-    this.setState({
-      cart: item,
-    });
+    if (this.state.uid !== "") {
+      this.setState({
+        cart: item,
+      });
+    } else {
+      swal("please login first Before add to cart");
+    }
   };
 
   showCart = () => {
@@ -81,25 +93,25 @@ class Dish extends Component {
         <div className="dishContainer">
           {this.state.dishData.length !== 0
             ? this.state.dishData.map((item, index) => {
-              return (
-                <div key={index} className="dish_box">
-                  <div className="dish_img_areaX">
-                    <img src={item.img} alt="Dish img" />
-                  </div>
-                  <div className="dish_data">
-                    <div className="dish_nameX">{item.name}</div>
-                    <div className="dish_priceX">Price: {item.price}</div>
-                    <button
-                      className="dish_addToCartX"
-                      onClick={() => this.addToCartHandlar(item)}
-                    >
-                      {" "}
+                return (
+                  <div key={index} className="dish_box">
+                    <div className="dish_img_areaX">
+                      <img src={item.img} alt="Dish img" />
+                    </div>
+                    <div className="dish_data">
+                      <div className="dish_nameX">{item.name}</div>
+                      <div className="dish_priceX">Price: {item.price}</div>
+                      <button
+                        className="dish_addToCartX"
+                        onClick={() => this.addToCartHandlar(item)}
+                      >
+                        {" "}
                         Add to Cart
                       </button>
+                    </div>
                   </div>
-                </div>
-              );
-            })
+                );
+              })
             : null}
         </div>
 
